556f5a6a12a684b9cafe93939ce1d1646888c212
Greg Wilson <greg.wilson@plot.ly>
2024-06-04 16:03:34 -0400

    feat: generate assay data and regularize filenames

--- src/server/make_assays.py
+++ src/server/make_assays.py
@@ -1,46 +1,43 @@
-"""Initialize database with previous experimental data."""
+'''Initialize database with previous experimental data.'''
 
 import argparse
 from datetime import date, datetime, timedelta
+import json
 from pathlib import Path
 import random
-import sqlite3
 import string
 
 from faker import Faker
 
-from assay_params import load_params
+from params import AssayParams, load_params
 
 EXPERIMENTS = {
-    "calibration": {"staff": [1, 1], "duration": [0, 0], "plates": [1, 1]},
-    "trial": {"staff": [1, 2], "duration": [1, 2], "plates": [2, 16]},
+    'calibration': {'staff': [1, 1], 'duration': [0, 0], 'plates': [1, 1]},
+    'trial': {'staff': [1, 2], 'duration': [1, 2], 'plates': [2, 16]},
 }
 FILENAME_LENGTH = 8
 
 
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (date, datetime)):
+            return obj.isoformat()
+
+
 def main():
-    """Main driver."""
-    args = parse_args()
-    params = load_params(args.params)
-    random.seed(params.seed)
-    fake = Faker(params.locale)
-
-    connection = create_tables(args)
-    fill_staff(params, connection, fake)
-    fill_experiments(params, connection, fake)
-    connection.commit()
-
-
-def create_tables(args):
-    """Create database tables."""
-    sql = Path(args.tables).read_text()
-    connection = sqlite3.connect(args.dbfile)
-    connection.executescript(sql)
-    return connection
+    '''Main driver.'''
+    options = parse_args()
+    random.seed(options.params.seed)
+    fake = Faker(options.params.locale)
+    result = {
+        'staff': make_staff(options.params, fake),
+        **make_experiments(options.params, fake)
+    }
+    save(options.outfile, result)
 
 
-def fill_experiments(params, connection, fake):
-    """Create experiments and their data."""
+def make_experiments(params, fake):
+    '''Create experiments and their data.'''
     kinds = list(EXPERIMENTS.keys())
     staff_ids = list(range(1, params.staff + 1))
     experiments = []
@@ -56,7 +53,7 @@ def fill_experiments(params, connection, fake):
             (experiment_id, kind, round_date(started), round_date(ended))
         )
 
-        num_staff = random.randint(*EXPERIMENTS[kind]["staff"])
+        num_staff = random.randint(*EXPERIMENTS[kind]['staff'])
         performed.extend(
             [(s, experiment_id) for s in random.sample(staff_ids, num_staff)]
         )
@@ -68,20 +65,21 @@ def fill_experiments(params, connection, fake):
 
     invalidated = invalidate_plates(params, plates)
 
-    connection.executemany("insert into experiment values (?, ?, ?, ?)", experiments)
-    connection.executemany("insert into performed values (?, ?)", performed)
-    connection.executemany("insert into plate values (null, ?, ?, ?)", plates)
-    connection.executemany("insert into invalidated values (?, ?, ?)", invalidated)
+    return {
+        'experiments': experiments,
+        'performed': performed,
+        'plate': plates,
+        'invalidated': invalidated
+    }
 
 
-def fill_staff(params, connection, fake):
-    """Create people."""
-    data = [(fake.first_name(), fake.last_name()) for _ in range(params.staff)]
-    connection.executemany("insert into staff values (null, ?, ?)", data)
+def make_staff(params, fake):
+    '''Create people.'''
+    return [(fake.first_name(), fake.last_name()) for _ in range(params.staff)]
 
 
 def invalidate_plates(params, plates):
-    """Invalidate a random set of plates."""
+    '''Invalidate a random set of plates.'''
     selected = [
         (i, p[1]) for (i, p) in enumerate(plates) if random.random() < params.invalid
     ]
@@ -96,50 +94,52 @@ def invalidate_plates(params, plates):
 
 
 def make_random_filename():
-    """Create a random filename generator."""
-    filenames = set([""])
-    result = ""
+    '''Create a random filename generator.'''
+    filenames = set([''])
+    result = ''
     while True:
         while result in filenames:
-            stem = "".join(random.choices(string.hexdigits, k=FILENAME_LENGTH)).lower()
-            result = f"{stem}.csv"
+            stem = ''.join(random.choices(string.hexdigits, k=FILENAME_LENGTH)).lower()
+            result = f'{stem}.csv'
         filenames.add(result)
         yield result
 
 
 def parse_args():
-    """Parse command-line arguments."""
+    '''Parse command-line arguments.'''
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dbfile", type=str, required=True, help="database file")
-    parser.add_argument("--params", type=str, required=True, help="parameter file")
-    parser.add_argument("--tables", type=str, required=True, help="SQL tables file")
-    return parser.parse_args()
+    parser.add_argument('--outfile', type=str, default=None, help='output file')
+    parser.add_argument('--params', type=str, required=True, help='parameter file')
+    options = parser.parse_args()
+    assert options.params != options.outfile, 'Cannot use same filename for options and parameters'
+    options.params = load_params(AssayParams, options.params)
+    return options
 
 
 def random_experiment_duration(params, kind):
-    """Choose random start date and end date for experiment."""
+    '''Choose random start date and end date for experiment.'''
     start = random.uniform(params.startdate.timestamp(), params.enddate.timestamp())
     start = datetime.fromtimestamp(start)
-    duration = timedelta(days=random.randint(*EXPERIMENTS[kind]["duration"]))
+    duration = timedelta(days=random.randint(*EXPERIMENTS[kind]['duration']))
     end = start + duration
     end = None if end > params.enddate else end
     return start, end
 
 
 def random_plates(params, kind, experiment_id, start_date, random_filename):
-    """Generate random plate data."""
+    '''Generate random plate data.'''
     return [
         (
             experiment_id,
             random_date_interval(start_date, params.enddate),
             next(random_filename),
         )
-        for _ in range(random.randint(*EXPERIMENTS[kind]["plates"]))
+        for _ in range(random.randint(*EXPERIMENTS[kind]['plates']))
     ]
 
 
 def random_date_interval(start_date, end_date):
-    """Choose a random end date (inclusive)."""
+    '''Choose a random end date (inclusive).'''
     if isinstance(start_date, date):
         start_date = datetime(*start_date.timetuple()[:3])
     choice = random.uniform(start_date.timestamp(), end_date.timestamp())
@@ -148,9 +148,18 @@ def random_date_interval(start_date, end_date):
 
 
 def round_date(raw):
-    """Round time to whole day."""
+    '''Round time to whole day.'''
     return None if raw is None else date(*raw.timetuple()[:3])
 
 
-if __name__ == "__main__":
+def save(outfile, result):
+    '''Save or show generated data.'''
+    as_text = json.dumps(result, indent=4, cls=DateTimeEncoder)
+    if outfile:
+        Path(outfile).write_text(as_text)
+    else:
+        print(as_text)
+
+
+if __name__ == '__main__':
     main()
