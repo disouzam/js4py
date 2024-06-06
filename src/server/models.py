b03f0532705c20bfe66c144b54a1f363fc1505fb
Greg Wilson <gvwilson@third-bit.com>
2024-06-05 21:03:23 -0400

    refactor: simplify date handling

--- src/server/models.py
+++ src/server/models.py
@@ -1,10 +1,18 @@
 '''Lab data models.'''
 
 from datetime import date as date_type
-from pydantic import field_serializer
 from sqlmodel import Field, Relationship, SQLModel
 
-class Site(SQLModel, table=True):
+
+class ModelWithDate(SQLModel):
+    '''Provide uniform date formatting.'''
+    class Config:
+        json_encoders = {
+            date_type: lambda dt: dt.isoformat() if dt is not None else ''
+        }
+
+
+class Site(ModelWithDate, table=True):
     '''Survey sites.'''
     site_id: str = Field(primary_key=True)
     lon: float
@@ -13,7 +21,7 @@ class Site(SQLModel, table=True):
     surveys: list['Survey'] = Relationship(back_populates='site')
 
 
-class Survey(SQLModel, table=True):
+class Survey(ModelWithDate, table=True):
     '''Surveys conducted.'''
     survey_id: int = Field(primary_key=True)
     site_id: str = Field(foreign_key='site.site_id')
@@ -22,12 +30,8 @@ class Survey(SQLModel, table=True):
     site: Site = Relationship(back_populates='surveys')
     samples: list['Sample'] = Relationship(back_populates='survey')
 
-    @field_serializer('date')
-    def serialize_date(self, dt, _info):
-        return dt.isoformat() if dt is not None else ''
 
-
-class Sample(SQLModel, table=True):
+class Sample(ModelWithDate, table=True):
     '''Individual samples.'''
     sample_id: int = Field(primary_key=True)
     survey_id: str = Field(foreign_key='survey.survey_id')
@@ -38,7 +42,7 @@ class Sample(SQLModel, table=True):
     survey: Survey = Relationship(back_populates='samples')
 
 
-class Staff(SQLModel, table=True):
+class Staff(ModelWithDate, table=True):
     '''Lab staff.'''
     staff_id: int = Field(primary_key=True)
     personal: str
@@ -48,7 +52,7 @@ class Staff(SQLModel, table=True):
     invalidated: list['Invalidated'] = Relationship(back_populates='staff')
 
 
-class Experiment(SQLModel, table=True):
+class Experiment(ModelWithDate, table=True):
     '''Experiments.'''
     sample_id: int = Field(primary_key=True)
     kind: str
@@ -58,16 +62,8 @@ class Experiment(SQLModel, table=True):
     performed: list['Performed'] = Relationship(back_populates='experiment')
     plates: list['Plate'] = Relationship(back_populates='experiment')
 
-    @field_serializer('start')
-    def serialize_date(self, dt, _info):
-        return dt.isoformat() if dt is not None else ''
 
-    @field_serializer('end')
-    def serialize_date(self, dt, _info):
-        return dt.isoformat() if dt is not None else ''
-
-
-class Performed(SQLModel, table=True):
+class Performed(ModelWithDate, table=True):
     '''Who did what experiments?'''
     staff_id: int = Field(foreign_key='staff.staff_id')
     sample_id: int = Field(foreign_key='experiment.sample_id')
@@ -77,7 +73,7 @@ class Performed(SQLModel, table=True):
     experiment: Experiment = Relationship(back_populates='performed')
 
 
-class Plate(SQLModel, table=True):
+class Plate(ModelWithDate, table=True):
     '''What experimental plates do we have?'''
     plate_id: int = Field(primary_key=True)
     sample_id: int = Field(foreign_key='experiment.sample_id')
@@ -87,12 +83,8 @@ class Plate(SQLModel, table=True):
     experiment: Experiment = Relationship(back_populates='plates')
     invalidated: list['Invalidated'] = Relationship(back_populates='plate')
 
-    @field_serializer('date')
-    def serialize_date(self, dt, _info):
-        return dt.isoformat() if dt is not None else ''
 
-
-class Invalidated(SQLModel, table=True):
+class Invalidated(ModelWithDate, table=True):
     '''Which plates have been invalidated?'''
     plate_id: int = Field(foreign_key='plate.plate_id')
     staff_id: int = Field(foreign_key='staff.staff_id')
@@ -101,7 +93,3 @@ class Invalidated(SQLModel, table=True):
     rowid: int = Field(primary_key=True)
     plate: Plate = Relationship(back_populates='invalidated')
     staff: Staff = Relationship(back_populates='invalidated')
-
-    @field_serializer('date')
-    def serialize_date(self, dt, _info):
-        return dt.isoformat() if dt is not None else ''
